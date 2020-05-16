381e141975ebb9b298315c3a4c0c9623eee85e7a
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-05-16 20:22:53 +0300

    refactoring colors module, it should return ord and view map for items in window. The item contains symbol ord + color ord

--- escape_from_maze/colors.py
+++ escape_from_maze/colors.py
@@ -1,26 +1,25 @@
 from escape_from_maze.global_vars import player_color, start_text_color, enemy_color
+from escape_from_maze.global_vars import enemy_view, player_view
 import curses
 
 
-def init_color_start_text():
-    number = start_text_color
-    curses.init_color(number, 855, 730, 150)
-    curses.init_pair(number, number, 0)
-
-
-def init_color_player():
-    number = player_color
-    curses.init_color(number, 455, 730, 750)
-    curses.init_pair(number, number, 0)
-
-
-def init_color_enemy():
-    number = enemy_color
-    curses.init_color(number, 800, 400, 400)
+def init_color(number: int, rgb: tuple):
+    curses.init_color(number, *rgb)
     curses.init_pair(number, number, 0)
 
 
 def init_colors():
-    init_color_player()
-    init_color_start_text()
-    init_color_enemy()
+    init_color(player_color, (455, 730, 750))
+    init_color(start_text_color, (855, 730, 150))
+    init_color(enemy_color, (800, 400, 400))
+
+
+def get_color_ord(number: int) -> int:
+    return curses.color_pair(number)
+
+
+def get_view_with_color_and_ord_map() -> dict:
+    return {
+        get_color_ord(player_color) + ord(player_view): player_view,
+        get_color_ord(enemy_color) + ord(enemy_view): enemy_view
+    }
--- start.py
+++ start.py
@@ -4,8 +4,9 @@ from escape_from_maze.enemies_generator import generate_enemies
 from escape_from_maze.enemies_manager.enemies_manager import EnemiesManager
 from escape_from_maze.units import Player
 from escape_from_maze.views import print_press_space_to_start, remove_press_space_to_start, print_enemies_amount
-from escape_from_maze.colors import init_colors
+from escape_from_maze.colors import init_colors, get_view_with_color_and_ord_map
 from escape_from_maze.global_vars import keys_map
+from time import sleep
 
 
 # temporary func for test
@@ -13,48 +14,67 @@ def print_h_w_window(window, height, width):
     window.addstr(0, 0, '{0}, {1}'.format(height, width))
 
 
-def main(window):
-    curses.curs_set(False)
-    window.nodelay(True)
-    init_colors()
+class Game:
+    def __init__(self):
+        self.enemies_manager = EnemiesManager()
+        self.player = None
+        self.window_height_width = (0, 0)
+        self.view_ord_map = {}
+        self.started = False
+        self.player = None
+        self.window = None
 
-    window_height_width = (0, 0)
+    def _prepare_game(self, window):
+        self.window = window
+        self.view_ord_map = get_view_with_color_and_ord_map()
+        self.window.nodelay(True)
+        curses.curs_set(False)
+        init_colors()
 
-    start = False
-    player = None
+    def start(self, window):
+        self._prepare_game(window)
 
-    enemies_manager = EnemiesManager()
+        while True:
+            height, width = window.getmaxyx()
+            if self.window_height_width[0] != height or self.window_height_width[1] != width:
+                self.window.clear()
+                coordinates = generate_maze(self.window, height, width)
+                self.window_height_width = height, width
 
-    while True:
-        height, width = window.getmaxyx()
-        if window_height_width[0] != height or window_height_width[1] != width:
-            window.clear()
-            coordinates = generate_maze(window, height, width)
-            window_height_width = height, width
+            print_h_w_window(self.window, height, width)
 
-        print_h_w_window(window, height, width)
+            if not self.started:
+                print_press_space_to_start(self.window, height, width)
 
-        if not start:
-           print_press_space_to_start(window, height, width)
+            key = window.getch()
 
-        key = window.getch()
+            if not self.started and key == 32:
+                self.started = True
+                remove_press_space_to_start(self.window, height, width)
+                x = coordinates[0] + 1
+                y = coordinates[2] + 1
+                self.player = Player(x, y, self)
 
-        if not start and key == 32:
-            start = True
-            remove_press_space_to_start(window, height, width)
-            x = coordinates[0] + 1
-            y = coordinates[2] + 1
-            player = Player(x, y, window)
-            enemies = generate_enemies(coordinates, window)
-            print_enemies_amount(window, len(enemies))
+                enemies = generate_enemies(coordinates, self)
+                print_enemies_amount(self.window, len(enemies))
 
-            enemies_manager.take_enemies(enemies)
-            enemies_manager.start()
+                self.enemies_manager.take_enemies(enemies)
+                self.enemies_manager.start()
 
-        elif start and key in keys_map.keys():
-            player.do_move(key)
+            elif self.started and key in keys_map.keys():
+                self.player.do_move(key)
 
-        window.refresh()
+            window.refresh()
+
+    def lose_game(self):
+        sleep(3)
+        self.window.erase()
+        self.enemies_manager.stop()
+        text = 'GAME OVER'
+        height, width = self.window_height_width
+        height = (height // 2) - 1
+        width = (width // 2) - 1 - len(text)//2
+        self.window.addstr(height, width, text)
 
 
-curses.wrapper(main)
+curses.wrapper(Game().start)
