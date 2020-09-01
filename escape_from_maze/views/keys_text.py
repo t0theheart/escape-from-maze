011c16f2e46c984405aa39952288ef5c4794e7ad
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-09-01 16:49:33 +0300

    fix!!!!

--- escape_from_maze/views/keys_text.py
+++ escape_from_maze/views/keys_text.py
@@ -4,16 +4,16 @@ import curses
 
 class KeysText:
     def __init__(self, window, total_keys):
-        self._window = window
-        self._total_keys = total_keys
-        self._collected_keys = 0
-        self._indent = maze_indents[0] + 2
-        self._indent_with_text = self._indent + keys_text_len
+        self.window = window
+        self.total_keys = total_keys
+        self.collected_keys = 0
+        self.indent = maze_indents[0] + 2
+        self.indent_with_text = self._indent + keys_text_len
 
     def print_keys_amount(self):
-        self._window.addstr(1, self._indent, keys_text.format(self._total_keys))
-        self._window.addstr(1, self._indent_with_text, f'{self._collected_keys}/{self._total_keys}', curses.color_pair(yellow))
+        self.window.addstr(1, self.indent, keys_text.format(self.total_keys))
+        self.window.addstr(1, self.indent_with_text, f'{self.collected_keys}/{self.total_keys}', curses.color_pair(yellow))
 
     def up_and_print_keys_amount(self):
-        self._collected_keys += 1
+        self.collected_keys += 1
         self.print_keys_amount()
